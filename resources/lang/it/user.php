30d6a12e735507bb6b71d1980a24d914dea29964
Gallo Mauro <mauro.gallo@polin.it>
2022-10-10 23:40:35 +0200

    lang: add Italian language

---
+++ resources/lang/it/user.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+    "resource" => [
+        "id" => "ID",
+        "single" => "Utente",
+        "email_verified_at" => "Email Verificata",
+        "created_at" => "Creato il",
+        "updated_at" => "Aggiornato il",
+        "verified" => "Verificato",
+        "unverified" => "Non Verificato",
+        "name" => "Nome",
+        "email" => "Email",
+        "password" => "Password",
+        "roles" => "Ruoli",
+        "label" => "Utenti",
+        "title" => [
+            "create" => "Crea Utente",
+            "edit" => "Modifica Utente",
+            "list" => "Utenti",
+            "home" => "Utenti"
+        ],
+    ]
+];
